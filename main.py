ec81cc0d8149238230ae653257ccb4d184f8067c
simon <simonkrey2009@icloud.com>
2025-10-13 10:41:06 +0200

    file structure

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import random as r
+import pygame as pg
 
-choices = ["rock", "paper", "scissors"]
-
-run = True
-while run:
-    cc = r.choice(choices)
-
-    uc = input("Please enter you're choice: ")
-    if uc.lower() in choices:
-        pass
-
-    else:
-        print("please choose one of these options: ")
